c6f3aaf60eaa64509524d3d619fc4d7ded1bc90b
milolang <milolang@gmail.com>
2015-05-18 17:25:40 -0400

    php connection

--- landing_page3.php
+++ landing_page3.php
@@ -2,14 +2,12 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Title of the document</title>
+<title>Landing Page</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"
-	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<script
-	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script
-	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link rel="stylesheet" href="main.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
 <body> 
 
@@ -32,15 +30,13 @@ function test_input($data) {
 ?>
 
 <h2>Request for The Son-Rise Program packet</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="form-inline" role="form">
-	<div class="form-group">
-		<label>First Name</label> 
-	   	<input type="text" name="firstName" class="form-control" placeholder="Enter First Name" required>	   	
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="form-inline form" role="form">
+	<div class="form-group">		
+	   	<input type="text" name="firstName" class="form-control floatleft" placeholder="Enter First Name" required>	 			
 	</div>
 	   
-	<div class="form-group">
-		<label>Last Name</label> 
-	   	<input type="text" name="lastName" class="form-control" placeholder="Enter Last Name" required>	   	
+	<div class="form-group"> 
+	   	<input type="text" name="lastName" class="form-control floatleft" placeholder="Enter Last Name" required>   	
 	</div> 
    <button type="submit" name="submit" class="btn btn-default">Submit</button> 	
 </form>
@@ -54,5 +50,25 @@ echo "<br>";
 
 ?>
 
+
+
+<?php
+$servername = "dg1oa45d0u.database.windows.net";
+$username = "thinkitdrinkitServer";
+$password = "@Agave2013";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
+
+
+
+		
 </body>
 </html>
